e4e0dfa25323ca2e792c1d97d355a98b9d30128e
Liz Stevenson <lizstevenson4@gmail.com>
2016-03-08 13:36:26 +0000

    Attempt to add bonus#2 to rock, paper scissors

--- programming_foundations/lesson_2/rock_paper_scissors.rb
+++ programming_foundations/lesson_2/rock_paper_scissors.rb
@@ -1,5 +1,21 @@
+# lizard/spock
+#  win
+# scissors => paper||lizard
+# paper    => rock||spock
+# rock     => scissors||lizard
+# lizard   => paper||spock
+# spock    => scissors||rock
 
-VALID_CHOICE = %w(rock paper scissors)
+# loose - one stage out.
+# scissors => rock||spock
+# paper    => scissors||lizard
+# rock     => paper||spock
+# lizard   => scissors||rock
+# spock    => paper||lizard
+
+
+
+VALID_CHOICE = {rock: "r", paper: "p", scissors: "s"}
 
 def prompt(message)
   puts "=> #{message}"
@@ -12,9 +28,9 @@ def win?(first, second)
 end
 
 def display_results(player_choice, computer_choice)
-  if win?(player_choice, computer_choice)
+  if win?(VALID_CHOICE.key(player_choice).to_s, computer_choice)
     prompt("You Win :) ")
-  elsif win?(computer_choice, player_choice)
+  elsif win?(computer_choice, VALID_CHOICE.key(player_choice).to_s)
     prompt("you loose :(")
   else
     prompt("You draw :|")
@@ -23,18 +39,18 @@ end
 
 loop do
   player_choice = ''
-  prompt("Please make a choice between: #{VALID_CHOICE.join(', ')}")
+  prompt("Please make a choice between: #{VALID_CHOICE.keys.join(', ')}")
 
   loop do
     player_choice = gets.chomp
-    if VALID_CHOICE.include?(player_choice)
+    if VALID_CHOICE.has_value?(player_choice)
       break
     else
       prompt("Sorry thats not available. Please choose either rock, paper or scissors")
     end
   end
 
-  computer_choice = VALID_CHOICE.sample
+  computer_choice = VALID_CHOICE.keys.sample
 
   prompt("the computer chose #{computer_choice}")
 
