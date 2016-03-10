b1cf05c8e8dfff4de22d47008d1e2103151833d6
Liz Stevenson <lizstevenson4@gmail.com>
2016-03-10 12:03:31 +0000

    Lesson4 tictaktoe the main game loop complete

--- programming_foundations/lesson_4/tictaktoe.rb
+++ programming_foundations/lesson_4/tictaktoe.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
+INITIAL_MARKER = ' '
+PLAYER_PIECE = 'X'
+COMPUTER_PIECE = 'O'
+
+def prompt(msg)
+  puts " => #{msg}"
+end
 
 def display_board(brd)
+  system "clear"
   puts "            "
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -17,9 +27,50 @@ end
 
 def initialize_board
   new_board = {}
-  initialize_board = (1..9).each { |num| new_board[num] = 'x'}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
+end
+
+# == IMPORTANT. NOT SETTING ASKING IF ITS EQUAL.
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt("Please select a square #{empty_squares(brd).join(', ')}:")
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt('Sorry thats not a valid choice')
+  end
+  brd[square] = PLAYER_PIECE
+end
+
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_PIECE
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  false
 end
 
 board = initialize_board
 display_board(board)
+
+loop do
+  player_places_piece!(board)
+  computer_places_piece!(board)
+  display_board(board)
+  break if someone_won?(board) || board_full?(board)
+end
+
+prompt('Thanks for playing see you next time')
+
+
+
