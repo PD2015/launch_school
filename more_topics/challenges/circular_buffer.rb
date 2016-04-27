52d4329937460e0685ee8f7811bba41dacefcf9a
Liz Stevenson <lizstevenson4@gmail.com>
2016-04-27 17:10:26 +0100

    More Topics challenges Circular buffer, refactor

--- more_topics/challenges/circular_buffer.rb
+++ more_topics/challenges/circular_buffer.rb
@@ -21,23 +21,24 @@ class BufferFullException < StandardError; end
     else
       return last_element
     end
- end
+  end
 
   def write(element)
-    raise BufferFullException if @buffer.size >= @size
-    @buffer << element unless element == nil
+    update_buffer(element) { raise BufferFullException }
   end
 
   def write!(element)
-    return write(element) if @buffer.size < @size
-    if @buffer.size >= @size && !(element == nil)
-      @buffer.shift
-      @buffer << element 
-    end
+    update_buffer(element) { @buffer.shift }
   end
-
+  
   def clear
     @buffer = []
+  end
+
+  def update_buffer(element)
+    return if element == nil
+    yield if @buffer.size >= @size 
+    @buffer << element 
   end
 
 end
