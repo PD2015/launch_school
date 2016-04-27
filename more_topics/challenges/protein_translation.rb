d2fd89bf39f133c103a2ccc758274dff524b0da4
Liz Stevenson <lizstevenson4@gmail.com>
2016-04-27 20:10:10 +0100

    More Topics challenges Protein translation R2

--- more_topics/challenges/protein_translation.rb
+++ more_topics/challenges/protein_translation.rb
@@ -19,39 +19,26 @@ class Translation
   end
 
   def self.of_rna(strand)
-    detected_aa = strand.scan(/.../).map { |c| self.of_codon(c) }
-     
-     self.codon_valid?(strand) 
-
+    detected_aa = strand.scan(/.../).map { |c| self.of_codon(c) } 
+    self.codon_valid?(strand) 
       if detected_aa.include?('STOP')
         index_of_stop = detected_aa.index('STOP')
         detected_aa.slice(0..(index_of_stop -1))
       else
         detected_aa
       end
-
- 
   end
 
   def self.codon_valid?(strand)
-
-    aa_candidates = strand.scan(/.../)
-
-
-    loop do 
-      codon = aa_candidates.shift
-      if AMINO_ACIDS.values.flatten.include?(codon) == false
-        raise InvalidCodonError 
-      else
-        return true if aa_candidates.empty?
-      end
+    strand.scan(/.../).each do |aa|
+      raise InvalidCodonError unless AMINO_ACIDS.values.flatten.include?(aa)
     end
-
+    return true
   end
 
 end
 
 
-# strand = 'carrot'
+# strand = 'AUG'
 # p Translation.of_rna(strand)
 # p Translation.of_codon('AUG')
