c90ecef1fdb004c0b25e3d9d9c549e515b01de81
Liz Stevenson <lizstevenson4@gmail.com>
2016-04-28 12:41:57 +0100

    More topics Challenges Octal refactor 1

--- more_topics/challenges/octal.rb
+++ more_topics/challenges/octal.rb
@@ -1,47 +1,28 @@
-require 'pry'
 class Octal
-  def initialize(number)
-    @number = number
+  def initialize(input)
+    @input = input
   end
 
   def to_decimal
     number_check
-    input_number_ary = @number.chars.map(&:to_i)
-    input_number_ary_size = input_number_ary.size
+    input_nums = @input.chars.map(&:to_i)
+    input_nums_size = input_nums.size
 
-    octal_no = input_number_ary.each_with_index.map do |number, index|
-      input_number_ary_size -= 1
-      number* (8**input_number_ary_size)
+    input_nums.map! do |num|
+      input_nums_size -= 1
+      num * (8**input_nums_size)
     end
-    octal_no.inject(:+)
+    input_nums.inject(:+)
   end
 
-
   private
+
   def number_check
-    # string contain 8 or 9?
-    if  (@number.chars.map(&:ord) & [56, 57]).any?
-      return @number = '0'
-     # string contain letter and number 1..7? 
-    elsif (@number.chars.map(&:ord) & (65..122).to_a).any? && 
-          (@number.chars.map(&:ord) & (1..7).to_a).any?
-      return @number = '0'
-    else
-      return 
-    end
+    input_as_ord = @input.chars.map(&:ord)
+    return unless (input_as_ord & [56, 57]).any? || 
+                  (input_as_ord & (65..122).to_a).any? && 
+                  (input_as_ord & (1..7).to_a).any?
+    return @input = '0' 
   end
 
 end
-
-
-
-
-
-
-# powers_array = (0..(input_number_ary_size-1)).to_a
-    # binding.pry
-    # octal_array = []
-    # input_number_ary_size.times do
-    # octal_array << [input_number_ary.shift, powers_array.pop]  
-    # end
-    # octal_array
