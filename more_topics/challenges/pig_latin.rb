fcfd59aa0f305bfd4d1caf61a698da27420c860e
Liz Stevenson <lizstevenson4@gmail.com>
2016-05-04 12:16:33 +0100

    More Topics Challenges PigLatin refactor 1

--- more_topics/challenges/pig_latin.rb
+++ more_topics/challenges/pig_latin.rb
@@ -2,26 +2,29 @@
 module PigLatin
   AY = 'ay'
   CHANGEABLE_VOWELS = /[uUyYxX]/
+  CONSONANT = /[^aeiou]/
+  VOWEL = /[aeiou]/
 
   def self.translate(string)
-    words_ary = string.split(/\W/)
-    words_ary.map! do |word|
-      char_ary = word.chars
-      loop do
+    string.split(/\W/)
+          .map! { |word| char_processing(word.chars) }
+          .join(' ')
+  end
+
+  def self.char_processing(char_ary)
+    loop do
         break if char_ary[0] =~ /[aeio]/
-        if char_ary[0] =~ CHANGEABLE_VOWELS && char_ary[1] =~ /[^aeiou]/
+        if char_ary[0] =~ CHANGEABLE_VOWELS && char_ary[1] =~ CONSONANT
           break
-        elsif  char_ary[0] =~ CHANGEABLE_VOWELS && char_ary[1] =~ /[aeiou]/
+        elsif  char_ary[0] =~ CHANGEABLE_VOWELS && char_ary[1] =~ VOWEL
           char_ary << char_ary.shift
           break
         else  
           char_ary << char_ary.shift
         end
       end
-      char_ary << AY
-      char_ary.join
-    end
-    words_ary.join(' ')
+    char_ary << AY
+    char_ary.join
   end
 end
 
